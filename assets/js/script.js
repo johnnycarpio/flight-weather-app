adfd9851fdfb24e407496551297bf837263d55b9
Johnny Carpio <johnnycarpio04@gmail.com>
2022-02-01 19:36:45 -0800

    working flight tracker API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,49 @@ var userOrigin = document.getElementById("startingpoint");
 var userDestination = document.getElementById("destination");
 var userDate = document.getElementById("flightdate");
 var buttonEl = document.getElementById("btn");
+var apiKey = "dbcad625cbmsh212b0e604919c31p12a3ffjsnba3ceac3d041";
 
-buttonEl.addEventListener("click", function() {
-    console.log(userOrigin.value)
-    console.log(userDestination.value);
-    console.log(userDate.value);
-});
+var flightHandler = function (event) {
+    event.preventDefault();
+    var code = userOrigin.value.trim();
+
+    if (code) {
+        userOrigin.value = "";
+    } else {
+        userOrigin.placeholder = "YOU MUST ENTER AIRPORT";
+    }
+
+    var dateLocal = userDate.value;
+    userDate.value = "";
+
+    getFlightInfo(code, dateLocal);
+
+};
+
+var getFlightInfo = function (code, dateLocal) {
+
+
+    var apiUrl = `https://aerodatabox.p.rapidapi.com/flights/number/${code}/${dateLocal}`
+
+    fetch(apiUrl, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+            "x-rapidapi-key": "dbcad625cbmsh212b0e604919c31p12a3ffjsnba3ceac3d041"
+        }
+    }).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    })
+
+
+};
+
+
+
+
+    buttonEl.addEventListener("click", flightHandler);
