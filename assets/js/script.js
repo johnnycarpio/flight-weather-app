6180f11b0705b067a26292c6d86d82e5c7c2943a
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-02-07 06:20:18 -0800

    display covid and flight info(partially)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,157 +1,162 @@
-var userOrigin = document.getElementById("startingpoint");
+var userInputEl = document.getElementById("input");
 var userDate = document.getElementById("flightdate");
 var buttonEl = document.getElementById("btn");
 var apiKey = "dbcad625cbmsh212b0e604919c31p12a3ffjsnba3ceac3d041";
 
 //Selects Flight Data card
-var flightInfo = document.querySelector('#flight_info');
+var flightInfo = document.querySelector("#flight_info");
 
 var flightHandler = function (event) {
-    event.preventDefault();
-    var code = userOrigin.value.trim();
+  event.preventDefault();
+  var code = userInputEl.value.trim();
 
-    if (code) {
-        userOrigin.value = "";
-    } else {
-        userOrigin.placeholder = "YOU MUST ENTER AIRPORT";
-    }
+  if (code) {
+    userInputEl.value = "";
+  } else {
+    userInputEl.placeholder = "YOU MUST ENTER AIRPORT";
+  }
 
-    var dateLocal = userDate.value;
-    userDate.value = "";
-
-    getFlightInfo(code, dateLocal);
-
+  getFlightInfo(code);
 };
 
 var getFlightInfo = function (code, dateLocal) {
+  var apiUrl = `https://aerodatabox.p.rapidapi.com/flights/number/${code}`;
 
-    var apiUrl = `https://aerodatabox.p.rapidapi.com/flights/number/${code}/${dateLocal}`
-
-    fetch(apiUrl, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-            "x-rapidapi-key": apiKey
-        }
-    }).then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                console.log(data);
-                getWeatherInfo(data[0].arrival.airport.municipalityName);
-                getState(data[0].arrival.airport.iata);
-                displayFlightData(data);
-            });
-        }
-    })
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+    },
+  }).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        getWeatherInfo(data[0].arrival.airport.municipalityName);
+        getState(data[0].arrival.airport.iata);
+        displayFlightData(data);
+      });
+    }
+  });
 };
 
 var getState = function (state) {
-    var apiUrl = `https://airport-info.p.rapidapi.com/airport?iata=${state}`;
+  var apiUrl = `https://airport-info.p.rapidapi.com/airport?iata=${state}`;
 
-    fetch(apiUrl, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "airport-info.p.rapidapi.com",
-		"x-rapidapi-key": apiKey
-	}
-})
-.then(function (response) {
-	if (response.ok) {
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "airport-info.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
         console.log(response);
-        response.json().then (function (stateData) {
-            console.log(stateData);
-            getCovidData(stateData.state);
-        })
-    }
-})
-.catch(err => {
-	console.error(err);
-});
-}
-
-
-
-var getCovidData = function (stateName) {
-    
-    var apiUrl = `https://covid-19-statistics.p.rapidapi.com/reports?region_province=${stateName}&iso=USA&region_name=US`
-
-    fetch(apiUrl, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
-            "x-rapidapi-key": apiKey
-        }
+        response.json().then(function (stateData) {
+          getCovidData(stateData.state);
+        });
+      }
     })
-    .then( function (response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then (function (covidData) {
-                console.log(covidData);
-                displayCovidData(covidData);
-            })
-        }
-        
-    })
-    .catch(err => {
-        console.error(err);
+    .catch((err) => {
+      console.error(err);
     });
-
 };
 
-var displayCovidData = function(covidData) {
-    confirmedCases = parseInt(covidData.data[0].confirmed);
-    activeCases = parseInt(covidData.data[0].active);
-    deathNumber = parseInt(covidData.data[0].deaths);
+var getCovidData = function (stateName) {
+  var apiUrl = `https://covid-19-statistics.p.rapidapi.com/reports?region_province=${stateName}&iso=USA&region_name=US`;
 
-    document.querySelector("#totalCaseNum").textContent = confirmedCases;
-    document.querySelector("#activeCaseNum").textContent = activeCases;
-    document.querySelector("#deathNum").textContent = deathNumber;
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (covidData) {
+          displayCovidData(covidData);
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+var displayCovidData = function (covidData) {
+  console.log(covidData);
+  confirmedCases = covidData.data[0].confirmed;
+  activeCases = covidData.data[0].confirmed_diff;
+  deathNumber = covidData.data[0].deaths;
+  deathDiff = covidData.data[0].deaths_diff;
+  stateName = covidData.data[0].region.province;
+
+  document.querySelector("#state-name").textContent = stateName;
+  document.querySelector("#total-cases").textContent = confirmedCases;
+  document.querySelector("#cases-diff").textContent = `Reported today`;
+  document.querySelector("#new-cases").textContent = activeCases;
+  document.querySelector("#new-cases-date").textContent = "Reported Today";
+  document.querySelector("#deaths").textContent = deathNumber;
+  document.querySelector(
+    "#deaths-diff"
+  ).textContent = `Reported Today + ${deathDiff}`;
 };
 
 var getWeatherInfo = function (city) {
+  var weatherUrl = `https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=${city}&contentType=json&unitGroup=us&shortColumnNames=0`;
 
-    var weatherUrl = `https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=${city}&contentType=json&unitGroup=us&shortColumnNames=0`
-
-    fetch(weatherUrl, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "visual-crossing-weather.p.rapidapi.com",
-            "x-rapidapi-key": apiKey
-        }
-    }).then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                console.log(data);
-                getState(data[0].arrival.airport.iata);
-            });
-        }
-    })
+  fetch(weatherUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "visual-crossing-weather.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+    },
+  }).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+        getState(data[0].arrival.airport.iata);
+      });
+    }
+  });
 };
 
-
 function displayFlightData(data) {
-    var flightStatus = data[0].status;
-    var iataName = data[0].departure.airport.name;
-    var departureTime = data[0].departure.scheduledTimeLocal;
-    var arrivalLocation = data[0].arrival.airport.name;
-    var planeModel = data[0].aircraft.model;
+  var flightStatus = data[0].status;
+  var cityNameDep = data[0].departure.airport.municipalityName;
+  var iataDepName = data[0].departure.airport.iata;
+  var departureTime = data[0].departure.scheduledTimeLocal;
+  var cityNameArr = data[0].arrival.airport.municipalityName;
+  var iataNameArr = data[0].arrival.airport.iata;
+  var planeModel = data[0].aircraft.model;
+  var flightStatus = data[0].status;
+  var timeFormat = departureTime.split(" ");
+  var formattedTime = new Date(timeFormat[0]).toLocaleDateString();
 
-    //Titles can be used for the data displayed
-    //var data_titles = ['Status', 'Departure City', 'Departure Time', 'Arrival City', 'Aircraft'];
-    var flightData = [flightStatus, iataName, departureTime, arrivalLocation, planeModel];
+  var statusListEl = document.getElementById("status-list");
+  document.getElementById("date").textContent = formattedTime;
+  var cityNameEl = document.createElement("li");
+  cityNameEl.classList.add("subtitle", "is-4", "has-text-centered");
+  cityNameEl.id = "flight-cities";
+//   var iconDepEl = document.createElement("i");
+//   iconDepEl.classList.add("fa-solid", "fa-plane-departure");
+//   var iconArrowEl = document.createElement("i");
+//   iconArrowEl.classList.add("fa-solid", "fa-arrow-alt-circle-right");
+//   var iconArrEl = document.createElement("i");
+//   iconArrEl.classList.add("fa-solid", "fa-plane-arrival");
+  
 
-    for(i=0; i<flightData.length; i++){
-        //Creates a list element for each item in the flight_data array
-        var info = document.createElement('li');
-        // info.textContent = data_titles[i] + ':' + flight_data[i]; //Can be used to display data with titles
-
-        //Sets the text content to the data returned from the API call
-        info.textContent = flightData[i];
-        flightInfo.append(info);
-    }
-
+  statusListEl.appendChild(cityNameEl);
+  cityNameEl.innerHTML = "<i class='fa-solid fa-plane-departure'></i>";
+  cityNameEl.textContent = `${cityNameDep}(${iataDepName})`;
+//   cityNameEl.appendChild('<i class="fa-solid fa-arrow-alt-circle-right"></i>');
+//   cityNameEl.appendChild('<i class="fa-solid fa-plane-arrival"></i>');
+  cityNameEl.textContent += `${cityNameArr}(${iataNameArr})`;
+  cityNameEl.innerHTML = `<i class="fa-solid fa-plane-departure"></i>${cityNameDep}(${iataDepName}) <i class="fa-solid fa-arrow-alt-circle-right"></i> <i class="fa-solid fa-plane-arrival"></i>${cityNameArr}(${iataNameArr})`
 }
 
 buttonEl.addEventListener("click", flightHandler);
